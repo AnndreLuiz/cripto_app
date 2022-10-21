63aff9362b1f71a4a7ddbfd55a1dd2a9025684b0
AnndreLuiz <andre.aqrt@gmail.com>
2022-10-21 17:48:20 -0300

    form

--- lib/pages/moedas_detalhes.dart
+++ lib/pages/moedas_detalhes.dart
@@ -1,5 +1,7 @@
 import 'package:criptomoedas_app/models/models.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class MoedasDetalhesPage extends StatefulWidget {
   Moeda moeda;
@@ -10,24 +12,65 @@ class MoedasDetalhesPage extends StatefulWidget {
 }
 
 class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  final TextEditingController _valor = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.moeda.nome),
       ),
-      body: Column(
-        children: [
-          Divider(),
-          Row(
-            children: [
-              SizedBox(
-                width: 50,
-                child: Image.asset(widget.moeda.icone),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 50,
+                    child: Image.asset(widget.moeda.icone),
+                  ),
+                  Container(width: 10),
+                  Text(
+                    real.format(widget.moeda.preco),
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: -1,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: _formKey,
+                child: TextFormField(
+                  controller: _valor,
+                  style: const TextStyle(fontSize: 22),
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Valor',
+                    prefixIcon: Icon(Icons.monetization_on_outlined),
+                    suffix: Text(
+                      'reais',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
