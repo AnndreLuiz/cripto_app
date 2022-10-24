12dc025cb1a0cfca324e2dc2474181debf08b3e3
AnndreLuiz <andre.aqrt@gmail.com>
2022-10-24 16:49:53 -0300

    provider

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:criptomoedas_app/pages/favoritas_page.dart';
+import 'package:criptomoedas_app/pages/moedas_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int paginaAtual = 0;
+  late PageController pc;
+
+  @override
+  void initState() {
+    super.initState();
+    pc = PageController(initialPage: paginaAtual);
+  }
+
+  setPaginaAtual(pagina) {
+    setState(() {
+      paginaAtual = pagina;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pc,
+        children: [
+          MoedasPage(),
+          FavoritasPage(),
+        ],
+        onPageChanged: setPaginaAtual,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: paginaAtual,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'todas'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favoritas'),
+        ],
+        onTap: (pagina) {
+          pc.animateToPage(pagina,
+              duration: Duration(milliseconds: 400), curve: Curves.ease);
+        },
+      ),
+    );
+  }
+}
