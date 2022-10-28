bffdab4f142caae01d1e5ab1e7ada1e1f47989ed
AnndreLuiz <andre.aqrt@gmail.com>
2022-10-28 17:41:54 -0300

    SQLite primeira parte finalizada

--- lib/pages/configuracoes_page.dart
+++ lib/pages/configuracoes_page.dart
@@ -1,15 +1,96 @@
+import 'package:criptomoedas_app/configs/app_settings.dart';
+import 'package:criptomoedas_app/repositories/conta_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class ConfiguracoesPage extends StatefulWidget {
-  const ConfiguracoesPage({super.key});
+  ConfiguracoesPage({Key? key}) : super(key: key);
 
   @override
-  State<ConfiguracoesPage> createState() => _ConfiguracoesPageState();
+  _ConfiguracoesPageState createState() => _ConfiguracoesPageState();
 }
 
 class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final conta = context.watch<ContaRepository>();
+    final loc = context.read<AppSettings>().locale;
+    NumberFormat real =
+        NumberFormat.currency(locale: loc['locale'], name: loc['name']);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Configuracoes'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('Saldo'),
+              subtitle: Text(
+                real.format(conta.saldo),
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.indigo,
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: updateSaldo,
+                icon: Icon(Icons.edit),
+              ),
+            ),
+            Divider(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  updateSaldo() async {
+    final form = GlobalKey<FormState>();
+    final valor = TextEditingController();
+    final conta = context.read<ContaRepository>();
+
+    valor.text = conta.toString();
+
+    AlertDialog dialog = AlertDialog(
+      title: Text('Atualizar o Saldo'),
+      content: Form(
+        key: form,
+        child: TextFormField(
+          controller: valor,
+          keyboardType: TextInputType.number,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(
+              RegExp(r'^\d+\.?\d*'),
+            ),
+          ],
+          validator: (value) {
+            if (value!.isEmpty) return 'Iforme o valor do Saldo';
+            return null;
+          },
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text('CANCELAR'),
+        ),
+        TextButton(
+          onPressed: () {
+            if (form.currentState!.validate()) {
+              conta.setSaldo(double.parse(valor.text));
+              Navigator.pop(context);
+            }
+          },
+          child: Text('SALVAR'),
+        )
+      ],
+    );
+
+    showDialog(context: context, builder: (context) => dialog);
   }
 }
